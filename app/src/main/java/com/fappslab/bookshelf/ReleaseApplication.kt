266b41873aafa3289457b45adfd042f3f960081c
F4bioo <costa.fbo@gmail.com>
2023-07-05 08:14:45 -0300

    Added: [BKSH/3-3] Setup Koin and Flipper

---
+++ app/src/main/java/com/fappslab/bookshelf/ReleaseApplication.kt
@@ -0,0 +1,40 @@
+package com.fappslab.bookshelf
+
+import android.app.Application
+import com.fappslab.bookshelf.di.InterceptorModule
+import com.fappslab.bookshelf.main.di.MainModule
+import com.fappslab.libraries.arch.di.ArchModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.KoinAppDeclaration
+import timber.log.Timber
+
+open class ReleaseApplication : Application() {
+
+    private val appDeclaration
+        get(): KoinAppDeclaration = {
+            Timber.plant(Timber.DebugTree())
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@ReleaseApplication)
+        }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(appDeclaration = appDeclaration)
+        koinLoad()
+    }
+
+    override fun onTerminate() {
+        stopKoin()
+        super.onTerminate()
+    }
+
+    private fun koinLoad() {
+        InterceptorModule.load()
+        ArchModule.load()
+        MainModule.load()
+    }
+}
